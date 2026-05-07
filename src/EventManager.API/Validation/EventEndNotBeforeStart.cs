7d220c9110fec3c4626098f96d365d8192869fe7
Ivan Ryazanov <rovany706@gmail.com>
2026-05-08 00:17:14 +0300

    Fix validation

--- src/EventManager.API/Validation/EventEndNotBeforeStart.cs
+++ src/EventManager.API/Validation/EventEndNotBeforeStart.cs
@@ -4,13 +4,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EventManager.API.Validation;
 
+/// <summary>
+/// Атрибут для валидации того, что дата конца мероприятия не раньше даты начала
+/// </summary>
 public class EventEndNotBeforeStart : ValidationAttribute
 {
+    public EventEndNotBeforeStart()
+    {
+        ErrorMessageResourceType = typeof(Resource);
+        ErrorMessageResourceName = nameof(Resource.ErrorEventEndBeforeStart);
+    }
+
+    /// <inheritdoc />
     public override bool IsValid(object? value)
     {
-        if (value is not EventDto eventDto)
+        if (value is not Event eventDto)
         {
-            return false;
+            return true;
         }
 
         return eventDto.StartAt < eventDto.EndAt;
