66ced421620b984257c53b8f730e8025dc4a7a67
Ivan Ryazanov <rovany706@gmail.com>
2026-06-26 02:03:59 +0300

    Add Booking entity

---
+++ src/EventManager.API/Models/Entities/BookingStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace EventManager.API.Models.Entities;
+
+/// <summary>
+/// Статус бронирования
+/// </summary>
+public enum BookingStatus
+{
+    /// <summary>
+    /// Бронь создана, ожидает обработки
+    /// </summary>
+    Pending,
+    
+    /// <summary>
+    /// Бронь подтверждена
+    /// </summary>
+    Confirmed,
+    
+    /// <summary>
+    /// Бронь отклонена
+    /// </summary>
+    Rejected
+}
